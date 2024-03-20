eb32368928678da8cedce5abefc90e85b51e660d
Den Hafiz <den.m.hafiz@gmail.com>
2024-03-20 23:24:05 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import os
+
+print("### This is a calculator app ###")
+print("")
+print("You can do addition, subtraction, division, and multiplication.")
+
+num_1 = input("Input the first number: ")
+num_2 = input("Input the second number: ")
